0a42cf3de9852304e680a6b343bde407021ac859
Luiz Guilherme <luizsett7@gmail.com>
2024-10-14 17:46:21 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
     ],
     server: {
-        host: '127.0.0.1',
-        port: 5173,
+        host: '0.0.0.0', // Allow access from any IP address (useful in Docker)
+        port: 5173,      // The port Vite is running on
+        cors: {
+            origin: 'http://localhost:8000', // Adjust to match your Laravel server URL
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+            allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+        },
     },
 });
