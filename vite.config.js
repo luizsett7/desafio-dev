3ae9abc9ea09b0913f3bb76f2d99224bc7a3b254
Luiz Guilherme <luizsett7@gmail.com>
2024-10-14 21:38:20 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', 
+        port: 5173,       
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        host: '0.0.0.0', 
-        port: 5173,      
-        cors: {
-            origin: 'http://localhost:8000',
-            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-            allowedHeaders: ['Content-Type', 'Authorization'], 
-        },
-    },
 });
